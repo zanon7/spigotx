09efc6b307cebacf66fb4193b2540ed74a9916dd
zanon <enzozanon2@gmail.com>
2024-05-30 03:20:18 -0300

    Smooth potting always

--- spigotx-server/src/main/java/net/minecraft/server/EntityProjectile.java
+++ spigotx-server/src/main/java/net/minecraft/server/EntityProjectile.java
@@ -141,6 +141,11 @@ public abstract class EntityProjectile extends Entity implements IProjectile {
                     AxisAlignedBB axisalignedbb = entity1.getBoundingBox().grow((double) f, (double) f, (double) f);
                     MovingObjectPosition movingobjectposition1 = axisalignedbb.a(vec3d, vec3d1);
 
+                    // Smooth Potting
+                    if (this instanceof EntityPotion && movingobjectposition1 == null && getBoundingBox().b(entity1.getBoundingBox())) {
+                        movingobjectposition1 = new MovingObjectPosition(entity1);
+                    }
+
                     if (movingobjectposition1 != null) {
                         double d1 = vec3d.distanceSquared(movingobjectposition1.pos);
 
